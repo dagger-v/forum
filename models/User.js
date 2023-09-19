a3aa7c8aa3edb9331e8aa2afc86c93a23cca1a6c
dagger-v <goingarcher@gmail.com>
2023-09-19 01:46:40 -0700

    updated index css

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+// Create User Schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: [true, "can't be blank"],
+    match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+    index: true,
+  },
+  password: String,
+  picture: {
+    type: String,
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+// hash password
+userSchema.plugin(passportLocalMongoose);
+
+// check if username / email is taken
+userSchema.plugin(uniqueValidator, { message: "is already taken." });
+
+module.exports = mongoose.model("User", userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "The Depths" });
+});
+
+router.get("/:id", function (req, res, next) {
+  const id = req.params.id;
+  res.render("topic", { title: "The Depths", id });
 });
 
 module.exports = router;
