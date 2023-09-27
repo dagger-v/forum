81641ed18fae1967694c81950cdb5a365deec838
dagger-v <goingarcher@gmail.com>
2023-09-26 21:44:07 -0700

    updated profile css

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+/* SETTINGS LISTINGS */
 router.get("/settings/signature", function (req, res, next) {
   res.render("signature", { title: "Yūgen Clan" });
+});
+
+/* PROFILE LISTINGS */
+router.get("/profile", function (req, res, next) {
+  res.render("profile", { title: "Yūgen Clan" });
 });
 
 module.exports = router;
